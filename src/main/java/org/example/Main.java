a00e1afdc247a2982685194fa8bc5b4cc88ca804
Gökhan Akyil <akyil88@outlook.de>
2024-05-07 11:09:41 +0300

    add to Localdate inTwoWeeks

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,10 +5,18 @@ import java.time.LocalTime;
 
 public class Main {
     public static void main(String[] args) {
+
+
+        // Aktuelles Datum
         LocalDate currentDate = LocalDate.now();
         System.out.println("Aktuelles Datum: " + currentDate);
 
+        // Aktuelle Uhrzeit
         LocalTime currentTime = LocalTime.now();
         System.out.println("Aktuelle Uhrzeit: " + currentTime);
+
+        //Datum mit Zeitspanne
+        LocalDate dateInTwoWeeks = currentDate.plusWeeks(2);
+        System.out.println("Datum in 2 Wochen: " + dateInTwoWeeks);
     }
 }
