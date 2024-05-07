ab42d92ee1284bf1be7617bc5994c0d39f1bb402
Gökhan Akyil <akyil88@outlook.de>
2024-05-07 11:05:14 +0300

    add Localdate and Localtime

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,14 @@
 package org.example;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        LocalDate currentDate = LocalDate.now();
+        System.out.println("Aktuelles Datum: " + currentDate);
+
+        LocalTime currentTime = LocalTime.now();
+        System.out.println("Aktuelle Uhrzeit: " + currentTime);
     }
 }
