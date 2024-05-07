d6e0026071ef1e1a5942a15efeb40474d1866edd
Gökhan Akyil <akyil88@outlook.de>
2024-05-07 11:50:44 +0300

    add to futuredate and comparison method

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -18,5 +18,26 @@ public class Main {
         //Datum mit Zeitspanne
         LocalDate dateInTwoWeeks = currentDate.plusWeeks(2);
         System.out.println("Datum in 2 Wochen: " + dateInTwoWeeks);
+
+        // Angegebenes zukünftiges Datum
+        LocalDate futuredate = LocalDate.of(2024,5,3);
+        System.out.println("Angegebenes zukünftiges Datum: " + futuredate);
+
+        if(futuredate.isBefore(currentDate)) {
+            System.out.println("Das aktuelle Datum liegt vor dem angegebenen zukünftigen Datum. ");
+        }else if(futuredate.isAfter(currentDate)) {
+            System.out.println("Das aktuelle Datum liegt nach dem angegebenen zukünftigen Datum. ");
+        }else {
+            System.out.println("Das aktuelle Datum ist das gleiche wie das angegbene zukünftige Datum. ");
+        }
+
+
+
+
+
+
+
+
+
     }
 }
